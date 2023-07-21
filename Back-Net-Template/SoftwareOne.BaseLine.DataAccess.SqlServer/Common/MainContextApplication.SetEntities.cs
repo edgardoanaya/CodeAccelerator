fe71f037dea6469179c5350a825747bfa0a83b8e
edagrdo anaya <edgardo.anaya@softwareone.com>
2023-07-20 21:14:17 -0500

    match default

--- Back-Net-Template/SoftwareOne.BaseLine.DataAccess.SqlServer/Common/MainContextApplication.SetEntities.cs
+++ Back-Net-Template/SoftwareOne.BaseLine.DataAccess.SqlServer/Common/MainContextApplication.SetEntities.cs
@@ -6,13 +6,10 @@ namespace SoftwareOne.BaseLine.DataAccess.SqlServer.Common
     /// 
     /// </summary>
     public partial class MainContextApplication
-    {
-        public virtual DbSet<Entities.City>? Cities { get; set; }
+    {        
         public virtual DbSet<Entities.Menu>? Menu { get; set; }
         public virtual DbSet<Entities.AuthorizationPermissions>? AuthorizationPermissions { get; set; }
-        public virtual DbSet<Entities.User>? Users { get; set; }
-        
+        public virtual DbSet<Entities.User>? Users { get; set; }        
         //{DbSet.Entities.City}
-
     }
 }
